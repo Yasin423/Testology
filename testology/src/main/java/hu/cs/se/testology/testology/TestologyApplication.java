7b675a47d94a46eead4463b68afbe6fc36829853
M.Yasin <yasin.mowlavezada423@gmail.com>
2019-10-18 12:10:53 +0430

    First commit pushing the local project

---
+++ testology/src/main/java/hu/cs/se/testology/testology/TestologyApplication.java
@@ -0,0 +1,13 @@
+package hu.cs.se.testology.testology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestologyApplication.class, args);
+	}
+
+}
