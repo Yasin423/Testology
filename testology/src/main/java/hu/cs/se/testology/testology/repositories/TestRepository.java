be826af99518637fca9e5eae69d470781969244c
Mohammad_Ozair <mohammad.ozair.cs@gmail.com>
2019-10-26 15:48:35 +0430

    Test repository is made

---
+++ testology/src/main/java/hu/cs/se/testology/testology/repositories/TestRepository.java
@@ -0,0 +1,8 @@
+package hu.cs.se.testology.testology.repositories;
+
+
+import hu.cs.se.testology.testology.model.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository extends JpaRepository<Test,Long> {
+}
